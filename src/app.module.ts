1d98302e87b6b7c7fcaa1171b13394b3189539ff
Gustavo <=>
2025-10-27 22:52:37 -0300

    Add initial nest project with typeorm (postgres)

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService): TypeOrmModuleOptions => ({
+        type: config.get<'postgres'>('DB_TYPE') as any,
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT') ?? '5432', 10),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: config.get<string>('DB_SYNCHRONIZE') === 'true',
+        logging: config.get<string>('DB_LOGGING') === 'true',
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class AppModule {}
